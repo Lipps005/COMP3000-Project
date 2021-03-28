8cf02618b2c4125e7c238884045a167f0733b845
lipps005 <samuellippett@gmail.com>
2021-03-28 23:52:26 +0100

    UI updates and major features
    - updated design and fixed draggable element issues.
    - simplified and improved styling
    - extra functionality for starting and pausing camera
    - App works offline
    
    Specifically for this commit, improvements have been made to the process of starting and managing the camera, which has also involved some UI changes. 
    Processing the frames has started to be moved into a WebWorker (separate thread) which has invloved the use of an OffscreenCanvas for rendering the ImageBitmap data.

--- static/appmain.js
+++ static/appmain.js
@@ -2,195 +2,247 @@
  * Author: Samuel Lippett
  * Project: COMP3000 Coursework
  */
+function CustomWorker()
+{
+    worker = new Worker("getFrameThread.js");
+    listeners = {};
 
-const event = new CustomEvent('capture', {detail: "capture event"});
+    this.postMessage = function(data, transfer = null)
+    {
+        if(transfer === null){worker.postMessage(data);}
+        else{worker.postMessage(data, transfer);}
+    }
+
+    this.addEventListener = function(name, listener)
+    {
+        listeners[name] = listener;
+    }
+
+    this.removeEventListener = function(name)
+    {
+        delete listeners[name];
+    }
+
+    worker.onmessage = function(event)
+    {
+        if(event.data.hasOwnProperty("customEvent") &&
+           event.data.hasOwnProperty("eventArguments"))
+        {
+            listeners[event.data.customEvent].apply(instance, event.data.eventArguments);
+        }
+    }
+
+    this.sendQuery = function()
+    {
+
+        worker.postMessage(
+        {
+            'customEvent': arguments[0],
+            'eventArguments': Array.prototype.slice.apply(arguments, 1)
+        });
+    }
+}
+
+const event = new CustomEvent('capture',{
+    detail: "capture event"
+});
 
 var t0;
 var t1;
 var track;
 var mediaStream;
 var imageCapture;
-function dispatchCaptureEvent()
-{
-   t0 = performance.now();
-   document.dispatchEvent(event);
+
+function dispatchCaptureEvent() {
+    t0 = performance.now();
+    document.dispatchEvent(event);
 
 }
-//dispatchCaptureEvent();
-
 var interval = setInterval(dispatchCaptureEvent, (1 / 20) * 1000);
 
-
-
-if ("serviceWorker" in navigator)
-{
-   var sworker = navigator.serviceWorker.register('service-worker.js');
+//create serviceWorker
+if ("serviceWorker"in navigator) {
+    var sworker = navigator.serviceWorker.register('service-worker.js');
 }
 
-function getCamera()
-{
-   return navigator.mediaDevices.getUserMedia({video: true})
-           .then(gotMedia)
-           .catch(err => console.error('getUserMedia() failed: ', err));
+//set up custom webworker and pass it an offscreen canvas.
+var WebWorker = new CustomWorker();
+
+
+//check if touch is supported in browser
+var tooolbarstartevent, toolbarmoveevent, toolbarendevent;
+if ("ontouchstart"in window) {
+    toolbarstartevent = "touchstart";
+    toolbarmoveevent = "touchmove";
+    toolbarendevent = "touchend";
+} else {
+    tooolbarstartevent = "mousedown";
+    toolbarmoveevent = "mousemove";
+    toolbarendevent = "mouseup";
+}
+
+function getCamera() {
+    return navigator.mediaDevices.getUserMedia({
+        video: {
+            facingMode: {
+                exact: "user"
+            }
+        }
+    }).then(gotMedia).catch(err=>console.error('getUserMedia() failed: ', err));
 }
 
 function gotMedia(mediastream) {
-   mediaStream = mediastream;
-   track = mediastream.getVideoTracks()[0];
-   imageCapture = new ImageCapture(track);
-   const video = document.createElement('video');
-   video.srcObject = mediaStream;
-   video.play();
-   track.onmute = function (evt)
-   {
-      evt.preventDefault();
-      console.log("muted");
-      $(document).unbind("capture");
+    mediaStream = mediastream;
+    track = mediastream.getVideoTracks()[0];
+    imageCapture = new ImageCapture(track);
 
-   };
+    //setting the srcObject of the video element
+    //means track isnt immediately muted when the 
+    //stream is paused.
+    const video = document.getElementById('hiddenvideo');
+    video.srcObject = mediaStream;
+    //video.play();
+    WebWorker.postMessage(
+    {customEvent: "transferCanvas",
+     width:track.getSettings().width,
+     height: track.getSettings().height});
 
-   track.onunmute = function (evt)
-   {
-      console.log("unmuted");
-      $(document).on("capture", captureFrame);
+    
+    track.onmute = function(evt) {
+        evt.preventDefault();
+        console.log("muted");
+        $(document).unbind("capture");
+        $("#loading-cover").toggleClass("load-out");
 
-   };
+    }
+    ;
 
-   track.ended = function (evt)
-   {
-      console.log("ended");
-   };
+    track.onunmute = function(evt) {
+        console.log("unmuted");
+        $(document).on("capture", captureFrame);
+        $("#loading-cover").toggleClass("load-out");
+
+    }
+    ;
+
+    track.ended = function(evt) {
+        console.log("ended");
+    }
+    ;
 }
 
-function captureFrame(e)
-{
-   if (track.readyState === "live" && track.muted === false && track.enabled === true)
-   {
-      imageCapture.grabFrame()
-              .then(processFrame)
-              .then(() => {
-                 t1 = performance.now();
-                 console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.");
-              })
-              .catch(err => console.error('grabFrame() failed: ', track.enabled));
+function captureFrame(e) {
+    if (track.readyState === "live" && track.muted === false && track.enabled === true) {
+        imageCapture.grabFrame().then(processFrame).then(()=>{
+            t1 = performance.now();
+            //console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.");
+        }
+        ).catch(err=>console.error('grabFrame() failed: ', track.enabled));
 
-   }
+    }
 }
-
 
 function processFrame(imgData) {
-   console.log("processing");
+    WebWorker.postMessage({customEvent:"transferImageBitmap",bitmap: imgData},[imgData]);
 }
 
+$(document).ready(function() {
 
+    function notifyOnline() {
+        $("#online-modal").removeClass("modal-notify");
+        void $("#online-modal").outerWidth();
+        $("#online-modal").addClass("modal-notify");
 
-$(document).ready(function () {
+    }
 
-   function notifyOnline() {
-      $("#online-modal").removeClass("modal-notify");
-      void $("#online-modal").outerWidth();
-      $("#online-modal").addClass("modal-notify");
+    function notifyOffline() {
+        $("#offline-modal").removeClass("modal-notify");
+        void $("#offline-modal").outerWidth();
+        $("#offline-modal").addClass("modal-notify");
+    }
 
-   }
+    window.addEventListener('online', notifyOnline);
+    window.addEventListener('offline', notifyOffline);
 
-   function notifyOffline() {
-      $("#offline-modal").removeClass("modal-notify");
-      void $("#offline-modal").outerWidth();
-      $("#offline-modal").addClass("modal-notify");
-   }
+    var pos1 = 0
+      , pos2 = 0
+      , pos3 = 0
+      , pos4 = 0;
 
-   window.addEventListener('online', notifyOnline);
-   window.addEventListener('offline', notifyOffline);
+    $("#toolbar-mover").on(toolbarstartevent, function(e) {
 
+        var event = e.changedTouches[0];
 
-   var pos1 = 0,
-           pos2 = 0,
-           pos3 = 0,
-           pos4 = 0;
+        // get the mouse cursor position at startup:
+        pos3 = event.layerX;
+        pos4 = event.layerY;
+        // call a function whenever the cursor moves:
+        $("body").on(toolbarmoveevent, mousemover);
 
-   $("#toolbar-mover").on("mousedown touchstart", function (e) {
+        $("body").on(toolbarendevent, mouseupevent);
 
-            var event = e.changedTouches[0] === undefined ? e : e.changedTouches[0];
+    });
 
-      // get the mouse cursor position at startup:
-       pos3 = event.clientX;
-      pos4 = event.clientY;
-      // call a function whenever the cursor moves:
-      $("body").on("mousemove touchmove", mousemover);
+    function mousemover(e) {
+        var event = e.changedTouches[0] === undefined ? e : e.changedTouches[0];
+        pos1 = pos3 - event.layerX;
+        pos2 = pos4 - event.layerY;
+        pos3 = event.clientX;
+        pos4 = event.clientY;
+        $("#toolbar").css("margin-bottom", ($("#toolbar-container").innerHeight() - pos4) + "px");
+        $("#toolbar").css("margin-right", ($("#toolbar-container").innerWidth() - pos3) + "px");
+    }
+    ;function mouseupevent() {
+        /* stop moving when mouse button is released:*/
+        $("body").unbind("mousemove touchmove");
+        $("body").unbind("mouseup touchend");
+    }
 
-      $("body").on("mouseup touchend", mouseupevent);
+    function userClickPause() {
+        console.log("pause");
+        try {
+            track.enabled = false;
+            $("#pause-button, #play-button").toggleClass("element-hidden");
+            $("#loading-cover").removeClass("load-out");
+        } catch (err) {
+            console.log("track not defined");
+        }
+    }
+    ;function userClickPlay() {
+        console.log("play");
+        try {
+            track.enabled = true;
+            $("#pause-button, #play-button").toggleClass("element-hidden");
+            $("#loading-cover").addClass("load-out");
+            
 
-   });
+        } catch (err) {
+            console.log("track not defined");
+        }
+    }
+    ;$(window).on("blur", userClickPause);
+    $("#pause-button").click(userClickPause);
+    $("#play-button").click(userClickPlay);
 
-   function mousemover(e) {
-      var event = e.changedTouches[0] === undefined ? e : e.changedTouches[0];
-      pos1 = pos3 - event.clientX;
-      pos2 = pos4 - event.clientY;
-      pos3 = event.clientX;
-      pos4 = event.clientY;
-		$("#toolbar").css("margin-bottom", ($("#toolbar-container").innerHeight() - pos4) + "px");
-		$("#toolbar").css("margin-right", ($("#toolbar-container").innerWidth() - pos3) + "px");
-   }
-   ;
+    $("#camera-show").click(async function() {
+        await getCamera();
+        $("#loading-cover").toggleClass("load-out");
+        $(document).on("capture", captureFrame);
+        userClickPlay();
+        $("#camera-hide, #camera-show").addClass("element-hidden");
 
-   function mouseupevent() {
-      /* stop moving when mouse button is released:*/
-      $("body").unbind("mousemove touchmove");
-      $("body").unbind("mouseup touchend");
-   }
+    });
 
-   function userClickPause()
-   {
-      console.log("pause");
-      try
-      {
-         track.enabled = false;
-         $("#pause-button, #play-button").toggleClass("element-hidden");
-      } catch (err)
-      {
-         console.log("track not defined");
-      }
-   };
+    var showToolbar = function() {
 
-   function userClickPlay()
-   {
-      console.log("play");
-      try
-      {
-         track.enabled = true;
-         $("#pause-button, #play-button").toggleClass("element-hidden");
+        $("#toolbar-body").toggleClass("roll");
+        $(this).toggleClass("roll");
 
-      } catch (err)
-      {
-         console.log("track not defined");
-      }
-   };
-   
-   $(window).on("blur", userClickPause);
-   $("#pause-button").click(userClickPause);
-   $("#play-button").click(userClickPlay);
+    };
 
+    $("#toolbar-mover").click(showToolbar);
 
-   $("#camera-show").click(async function ()
-   {
-      await getCamera();
-      $(document).on("capture", captureFrame);
-      userClickPlay();
-      $("#camera-hide, #camera-show").toggleClass("element-hidden");
-
-   });
-
-   var showToolbar = function () {
-
-      $("#toolbar-body").toggleClass("roll");
-      $(this).toggleClass("roll");
-
-   };
-
-   $("#toolbar-mover").click(showToolbar);
-
-   $(window).resize(function ()
-   {
-      $("#toolbar").css("top", $("body").height() - $("toolbar").outerWidth(true) + "px");
-   });
+    $(window).resize(function() {
+        $("#toolbar").css("top", $("body").height() - $("toolbar").outerWidth(true) + "px");
+    });
 });
--- static/getFrameThread.js
+++ static/getFrameThread.js
@@ -2,11 +2,44 @@
  * Author: Samuel Lippett
  * Project: COMP3000 Coursework
  */
+var canvas;
+var width, height;
+var queryableFunctions = {
+    "transferCanvas" : function(evt)
+    {
+      width = evt.data.width;
+      height = evt.data.height;
+      canvas = new OffscreenCanvas(width, height);
+      console.log(canvas);
 
-var imageCapture = navigator.mediaDevices;
+    },
+
+    "transferImageBitmap" : function(evt)
+    {
+      /*
+      getting the context of the canvas using bitmaprenderer returns
+      an ImageBitmapRenderingContext object. 
+      This is a "performance-orientated interface" that minimises memory
+      overhead such as copying data, and improves performance by avoiding
+      "intermediate compositing"; that is, for example, by not pre-rendering
+      the imageData with the alpha background to create the appearance of 
+      transparency. However, using the bitmaprenderer context peramenantly 
+      binds this context to the canvas, meaning you are unable to access the
+      image data.
+      var context = canvas.getContext("bitmaprenderer");
+       console.log(evt.data.bitmap);
+      context.transferFromImageBitmap(evt.data.bitmap);
+      console.log(canvas); 
+      */
+
+      canvas.getContext("2d").drawImage(evt.data.bitmap, 0, 0);
+      var data = canvas.getContext("2d").getImageData(0, 0, width, height);
+    }
+};
 
 
-
-
-
+self.onmessage = function(evt)
+{
+  queryableFunctions[evt.data.customEvent].call(self, evt);
+}
 
